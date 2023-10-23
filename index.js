ca8e9ae3135b4122dc101ffcf380a1274418fe97
Ram2721 <ribenitez@up.edu.ph>
2023-10-23 12:24:51 +0800

    added index.js file

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import router from './router.js';
+
+//instantiate express library
+const app=express();
+app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+
+console.log("Server now running on port 3000");
+app.listen(3000);
+//create a router file
+router(app);
